9f4a4dcb4d3c5223fef35732eac43f43d11fdaf9
Aaron Contreras <aaronlcaq@gmail.com>
2021-12-03 13:08:16 -0500

    Finished Gatsby tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+	return (
+		<Layout pageTitle="About Me">
+			<p>Hey y'all! I'm Aaron, my girlfriend and my sister call me "babies", and I'm learning React through Gatsby in order to create a set of static pages for my Rails app.</p>
+		</Layout>
+	)
+}
+
+export default AboutPage
