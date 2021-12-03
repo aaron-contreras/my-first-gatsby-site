83a335c85d2f25eecf89830c6ee1105beb807739
Aaron Contreras <aaronlcaq@gmail.com>
2021-12-03 13:54:58 -0500

    Finished gatsby tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
 	return (
 		<Layout pageTitle="Home Page">
 			<p>I'm making this by following the Gatsby Tutorial.</p>
+			<StaticImage
+				alt="A description of what a 'tubby' is"
+			src="../images/tubby.png"
+			/>
 		</Layout>
 	)
 }
